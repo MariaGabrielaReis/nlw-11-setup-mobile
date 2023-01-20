e76fb3f1625c7cdb551ee5d784966b6b70b628a4
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-20 09:52:40 -0300

    feat: create and use home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,29 @@
+import { View, Text } from 'react-native';
+
+import { HabitDay, DAY_SIZE } from '../components/HabitDay';
+import { Header } from '../components/Header';
+
+const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+export function Home() {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <Header />
+
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, index) => {
+          return (
+            <Text
+              key={index}
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{ width: DAY_SIZE }}
+            >
+              {weekDay}
+            </Text>
+          );
+        })}
+      </View>
+      <HabitDay />
+    </View>
+  );
+}
