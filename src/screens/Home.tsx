b0df71a9a9a3304db27b0600f30040fdb448710f
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-20 10:07:09 -0300

    feat: generate days to populate home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
 
@@ -7,6 +7,8 @@ import { Header } from '../components/Header';
 
 const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 const datesFromYearStart = generateDatesFromYearBeginning();
+const minimumSummaryDateSizes = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDateSizes - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -27,11 +29,24 @@ export function Home() {
         })}
       </View>
 
-      <View className="flex-row flex-wrap">
-        {datesFromYearStart.map(date => (
-          <HabitDay key={date.toISOString()} />
-        ))}
-      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map(date => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
